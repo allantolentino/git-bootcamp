11fbd1c80bccc48a3f4fe1a61cc3a1042613037b
angielyn.rivera <angielyn.rivera@SVPHLP00674>
2022-06-06 16:58:27 +0800

    Updated changes Program.cs

--- MyConsoleApp/MyConsoleApp/Program.cs
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -2,3 +2,8 @@
 Console.WriteLine("Welcome to source control using git!");
 
 Console.WriteLine("Paburger naman sir Chiz");
+
+Console.Write("Please enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+Console.WriteLine("Hi " + name);
